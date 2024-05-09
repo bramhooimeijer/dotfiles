44030b87d2f3038cb8dae1d47a45592494468774
Bram Hooimeijer <33625980+bramhooimeijer@users.noreply.github.com>
2024-05-09 15:03:32 +0200

    Update config

--- .config/sway/scripts/lock.sh
+++ .config/sway/scripts/lock.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 /home/braho/.config/sway/scripts/cliphist_clear.sh
-swaylock -e -F -l -c 000000
+if [ ! -f /home/braho/.images/wallpaper_blur.jpg ]; then
+	convert /home/braho/images/wallpaper.jpg -blur 0x8 /home/braho/.images/wallpaper_blur.jpg
+fi
+if ! pgrep -x swaylock; then swaylock -e -F -l -i /home/braho/.images/wallpaper_blur.jpg; fi
